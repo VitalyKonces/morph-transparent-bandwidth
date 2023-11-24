02dde033b73001203db6c0f8bd7b61a8a61746b2
VitalyKonces <140142399+VitalyKonces@users.noreply.github.com>
2023-11-24 07:25:36 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,43 +1,14 @@
-
-def main():
-        print(f"Random Number: {item}")
 import random
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    main()
-    for item in data:
-
-    data = generate_random_data()
-if __name__ == "__main__":
-    return data
-def generate_random_data():
-    }
-
-
-}
-function main() {
-    });
-        data.push(Math.floor(Math.random() * 100) + 1);
-main();
-function generateRandomData() {
-    const data = generateRandomData();
-    const data = [];
-    data.forEach(item => {
-        console.log("Random Number: " + item);
-    return data;
-    for (let i = 0; i < 10; i++) {
-}
-    for item in data:
-def main():
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-    main()
-import random
-    return data
-        print(f"Random Number: {item}")
-    data = generate_random_data()
-
-
 
 
 def generate_random_data():
+    return data
+
+
+    main()
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+        print(f"Random Number: {item}")
+    for item in data:
+if __name__ == "__main__":
