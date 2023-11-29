b1c913585a65e3a7ef51e410e26c4f2a36147195
VitalyKonces <140142399+VitalyKonces@users.noreply.github.com>
2023-11-29 21:18:17 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,46 @@
+
+def main():
+def generate_random_data():
+
+    data = generate_random_data()
+    return data
+    for item in data:
+if __name__ == "__main__":
+
+    main()
+import random
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]    for (let i = 0; i < 10; i++) {
+}
+function main() {
+    });
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+}
+    return data;
+    data.forEach(item => {
+    const data = [];
+main();
+
+    const data = generateRandomData();
+
+function generateRandomData() {
+    }
+        console.log("Random Number: " + item);    for (let i = 0; i < 10; i++) {
+    const data = [];
+    }
+    const data = generateRandomData();
+function generateRandomData() {
+    return data;
+    });
+        console.log("Random Number: " + item);
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+
+
+}
+main();
+}
+    data.forEach(item => {
